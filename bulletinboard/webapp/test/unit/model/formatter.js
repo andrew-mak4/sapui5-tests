23a2dd6186267d4287c5e94e9ad91d5821e0aaa2
andrew-mak4 <129939590+andrew-mak4@users.noreply.github.com>
2023-04-12 17:50:28 +0300

    step 2 - first unit test + notes

--- bulletinboard/webapp/test/unit/model/formatter.js
+++ bulletinboard/webapp/test/unit/model/formatter.js
@@ -34,4 +34,60 @@ sap.ui.define([
 	QUnit.test("Should round a zero", function (assert) {
 		numberUnitValueTestCase.call(this, assert, "0", "0.00");
 	});
+
+	QUnit.module("Price State");
+
+	// We could write a test checking the result of the formatter for each of numberUnitValue test cases but we do not want to repeat ourselves
+	// (DRY) – neither in the tests nor in the application coding – so we create a reuse function called priceStateTestCase.
+	// In this function, we call the formatter with the arguments provided as oOptions and make a strictEqual assertion for the expected parameter.
+
+	function priceStateTestCase(oOptions) {
+		// Act
+		var sState = formatter.priceState(oOptions.price);
+
+		//Assert
+		oOptions.assert.strictEqual(sState, oOptions.expected, "The price state was correct")
+	}
+
+	// The assert object – a special object injected by QUnit – is passed on as a reference to the function.
+	// QUnit is loaded once for the whole unit testing part of the app.
+	QUnit.test("Should format the products with a price lower than 50 to Success", function (assert) {
+		priceStateTestCase.call(this, {
+			assert: assert,
+			price: 42,
+			expected: "Success"
+		});
+	});
+
+	QUnit.test("Should format the products with a price of 50 to Normal", function (assert) {
+		priceStateTestCase.call(this, {
+			assert: assert,
+			price: 50,
+			expected: "None"
+		});
+	});
+
+	QUnit.test("Should format the products with a price between 50 and 250 to Normal", function (assert) {
+		priceStateTestCase.call(this, {
+			assert: assert,
+			price: 112,
+			expected: "None"
+		});
+	});
+
+	QUnit.test("Should format the products with a price between 250 and 2000 to Warning", function (assert) {
+		priceStateTestCase.call(this, {
+			assert: assert,
+			price: 798,
+			expected: "Warning"
+		});
+	});
+
+	QUnit.test("Should format the products with a price higher than 2000 to Error", function (assert) {
+		priceStateTestCase.call(this, {
+			assert: assert,
+			price: 2001,
+			expected: "Error"
+		});
+	});
 });
